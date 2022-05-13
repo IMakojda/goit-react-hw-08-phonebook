ccde82a2f2153f0a1abfe268e49293382a3f3fee
IMakojda <86243683+IMakojda@users.noreply.github.com>
2022-05-13 15:37:09 +0000

    Deploying to gh-pages from @ IMakojda/goit-react-hw-08-phonebook@f9496051541fc03b6e4d019e7e993a7acd628024 🚀

---
+++ static/js/512.04cdbcbd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[512],{6512:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var o=n(3504),r=n(184);function c(){return(0,r.jsxs)("h1",{children:["404 Page not found ",(0,r.jsx)("br",{}),":(  Please go to Contact "," ",(0,r.jsx)(o.rU,{to:"/contacts",children:"Contacts"})]})}}}]);
+//# sourceMappingURL=512.04cdbcbd.chunk.js.map
