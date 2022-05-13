f9496051541fc03b6e4d019e7e993a7acd628024
IMakojda <86243683+IMakojda@users.noreply.github.com>
2022-05-13 18:35:34 +0300

    contacts redirect

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <h1>
-      404 Page not found <br />:(  Please Login {' '}
-      <Link to="/contacts">Login</Link>
+      404 Page not found <br />:(  Please go to Contact {' '}
+      <Link to="/contacts">Contacts</Link>
     </h1>
   );
 };
